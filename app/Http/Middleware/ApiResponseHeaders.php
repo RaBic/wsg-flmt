5280efa411ff4c3cfe68fbfe7397436f1d2e793e
RaBic <rbickel@bitdesign.de>
2025-04-01 00:37:47 +0200

    fix: phpstan induced issues

--- app/Http/Middleware/ApiResponseHeaders.php
+++ app/Http/Middleware/ApiResponseHeaders.php
@@ -4,13 +4,14 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Carbon;
 
 class ApiResponseHeaders
 {
-    public function handle(Request $request, Closure $next): JsonResponse|Response
+    public function handle(Request $request, Closure $next): JsonResponse|Response|RedirectResponse
     {
         // Get the initial method sent by client
         $initialMethod = $request->method();
--- app/Http/Middleware/CustomizeFilament.php
+++ app/Http/Middleware/CustomizeFilament.php
@@ -3,13 +3,14 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class CustomizeFilament
 {
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
     {
         if (Auth::user() && ! Auth::user()->hasRole('super_admin')) {
             config(['filament-breezy.enable_sanctum' => false]);
