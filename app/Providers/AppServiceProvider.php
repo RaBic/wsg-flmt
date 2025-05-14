04cd4b3efc0cda455fbf566b890aae382f6a9612
RaBic <rbickel@bitdesign.de>
2025-05-14 23:49:47 +0200

    feat: persist table search/sort/filter in session

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
+use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
@@ -30,6 +31,14 @@ class AppServiceProvider extends ServiceProvider
             Css::make('custom-stylesheet', __DIR__ . '/../../resources/css/customize-fi-panel.css'),
             Js::make('custom-script', __DIR__ . '/../../resources/js/customize-fi-panel.js'),
         ]);
+
+        Table::configureUsing(function (Table $table): void {
+            $table->striped()
+                ->persistFiltersInSession()
+                ->persistSearchInSession()
+                ->persistSortInSession();
+        });
+
         /* Model::shouldBeStrict(!$this->app->environment('production')); */
         Model::preventLazyLoading(! $this->app->environment('production'));
         /* Model::preventSilentlyDiscardingAttributes(!$this->app->environment('production')); */
